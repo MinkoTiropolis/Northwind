cb2c293003e659017b69e3a916bf940ff0e898c3
Minko Tiropolis <minko.tiropolis@kontrax.bg>
2026-06-30 22:49:53 +0300

    feat: database scaffold

--- Northwind.Api/Program.cs
+++ Northwind.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Northwind.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Read the Northwind connection string from appsettings.json
+builder.Services.AddDbContext<NorthwindContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Northwind")));
 
 var app = builder.Build();
 
